31fe9fc01ffc865c4c15803717e188a8a7402ded
Вадим Воєвода <vadimvoyevoda@step.itstep.rv.ua>
2015-11-11 20:28:35 +0200

    Add ProjectStructure

--- PC.WebUI/src/app.js
+++ PC.WebUI/src/app.js
@@ -3,20 +3,20 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Public Control';
     config.map([
-      { route: ['', 'login'],    name: 'login',      moduleId: 'login',      nav: true, title: 'Login' },
-      { route: 'about',         name: 'about',     moduleId: 'about',     nav: true, title: 'About' }
+      { route: ['', 'login'],    name: 'login',      moduleId: 'routes/login/login',   nav: true, title: 'Login' },
+      { route: 'about',          name: 'about',      moduleId: 'routes/about/about',   nav: true, title: 'About' }
     ]);
 
     this.router = router;
   }
 
-  attached() {
-              var mapProp = {
-                  center:new google.maps.LatLng(50.6143691,26.2632106),
-                  zoom:13
-          };
-          var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-          google.maps.event.addDomListener(window, 'load', initialize);
-}
+//  attached() {
+//              var mapProp = {
+//                  center:new google.maps.LatLng(50.6143691,26.2632106),
+//                  zoom:13
+//          };
+//          var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+//          google.maps.event.addDomListener(window, 'load', initialize);
+//}
 
 }
