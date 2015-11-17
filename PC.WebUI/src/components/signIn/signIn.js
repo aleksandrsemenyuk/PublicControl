97264a6d9ab7147d693e1a0191c8434c3440b3e0
vadimvoyevoda <vadimvoyevoda@gmail.com>
2015-11-17 18:51:00 +0200

    AccountControllerStart

--- PC.WebUI/src/components/signIn/signIn.js
+++ PC.WebUI/src/components/signIn/signIn.js
@@ -1,10 +1,22 @@
 ﻿import {customElement, bindable} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-http-client';
 
 @customElement('signin')
 export class SignIn {
   @bindable title;
 
-    speak(){
+  static inject = [HttpClient];
+  constructor(http){
+      this.http = http;
+  }
+
+  getSome()
+  {
+      var res = this.http.get("api/Account/Some");
+      return res;
+  }
+    
+  speak(){
         alert(`Hello ${this.login} ${this.password}!`);
-}
+    }
 }
--- PC.WebUI/src/routes/login/login.js
+++ PC.WebUI/src/routes/login/login.js
@@ -1,3 +1,3 @@
-export class CustomerLogin{
+export class CustomerLogin {
+    
 }
-
