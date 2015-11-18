4bf28c672c83c9d4fe3f41eafbb50377333c6b6b
sergiyDyadenchuk <sergiyDyadenchuk@outlook.com>
2015-11-18 22:31:07 +0200

    implement generic repository and unit of work

---
+++ DataAccess/PCDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Database;
+
+namespace DataAccess
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext()
+            : base("name=DbConnectionString")
+        {
+        }
+
+        public new IDbSet<TEntity> Set<TEntity>() where TEntity : BaseEntity
+        {
+            return base.Set<TEntity>();
+        }
+    }
+}
--- PC.WebUI/Controllers/HomeController.cs
+++ PC.WebUI/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataAccess;
+using Database.Entities;
 
 namespace PC.WebUI.Controllers
 {
@@ -16,6 +18,59 @@ namespace PC.WebUI.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        //For testing
+        private UnitOfWork unitOfWork = new UnitOfWork();
+        private Repository<Entity> bookRepository;
+
+        public HomeController()
+        {
+            bookRepository = unitOfWork.Repository<Entity>();
+        }
+
+        public ActionResult CreateEditBook(int? id)
+        {
+            Entity model = new Entity();
+            if (id.HasValue)
+            {
+                model = bookRepository.GetById(id.Value);
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult CreateEditBook(Entity model)
+        {
+            var editModel = bookRepository.GetById(model.Id);
+
+            return View(model);
+        }
+
+        public ActionResult DeleteBook(int id)
+        {
+            Entity model = bookRepository.GetById(id);
+            return View(model);
+        }
+
+        [HttpPost, ActionName("DeleteBook")]
+        public ActionResult ConfirmDeleteBook(int id)
+        {
+            Entity model = bookRepository.GetById(id);
+            bookRepository.Delete(model);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DetailBook(int id)
+        {
+            Entity model = bookRepository.GetById(id);
+            return View(model);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            unitOfWork.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
