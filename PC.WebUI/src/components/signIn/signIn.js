57e304be6b5f7f4f1d77e7e9ef91a310f0667e8d
= <andrii.marusiak@gmail.com>
2015-11-19 19:41:47 +0200

    Login Validation - 2015-11-19

--- PC.WebUI/src/components/signIn/signIn.js
+++ PC.WebUI/src/components/signIn/signIn.js
@@ -1,22 +1,58 @@
-﻿import {customElement, bindable} from 'aurelia-framework';
+﻿
+import {Validation} from 'aurelia-validation';
+import {ensure} from 'aurelia-validation';
+import {inject} from 'aurelia-framework';
+import {customElement, bindable} from 'aurelia-framework';
 //import {HttpClient} from 'aurelia-http-client';
 
 @customElement('signin')
-export class SignIn {
-  @bindable title;
+//@inject(Validation)
+//export class RegisterViewModel {
+export class App {
+    @bindable title;
+    static inject() { return [Validation]; }
 
-  //static inject = [HttpClient];
-  //constructor(http){
-  //    this.http = http;
-  //}
+    //@ensure(function(it){ it.isNotEmpty().hasLengthBetween(3,10) })
+    //login = '';
 
-  getSome()
-  {
-      //var res = this.http.get("api/Account/Some");
-      return res;
-  }
-    
-  speak(){
-        alert(`Hello ${this.login} ${this.password}!`);
+    //@ensure(function(it){ it.isNotEmpty().hasLengthBetween(3,10).isStrongPassword(4) })
+    //  password = '';
+
+    constructor(validation) {
+        this.heading = 'Aurelia Login Validation !';
+        this.login = 'login';
+        this.password = '*****';
+
+        //this.validation = validation.on(this);
+        this.validation = validation.on(this)
+                    .ensure('login')
+                    .isNotEmpty()
+                    .hasLengthBetween(3,10)
+                    .ensure('password')
+                    .isNotEmpty()
+                    .hasLengthBetween(3,10)
+                    .isStrongPassword(4);
     }
+
+    register(){
+        //check if the validation is valid before performing the register
+        this.validation.validate().then( () => {
+            alert('Welcome!'); //Your more meaningful stuff would go here
+
+        var identity = { login : this.login, password:this.password };
+        var jsonObj = JSON.stringify( identity );
+			
+        var html = "";
+        for( var key in jsonObj ){
+            //html += "<br> &nbsp; &nbsp; &nbsp; "+ 
+            //    key +" = " + jsonObj[key] +
+            //    " &nbsp; &nbsp; &nbsp;(" 
+            //    + typeof( jsonObj[key] ) +")";
+            html += jsonObj[key] ;
+        }
+        document.getElementsByTagName("p")[0].innerHTML = html;
+
+    });
+}
+
 }
